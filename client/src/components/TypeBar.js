a2391fb686e448d7e340119229b1ca228eee22cd
Mikhail Zherdev <mikhail.zherdev@storecast.de>
2021-09-29 16:36:10 +0200

    init commit. client part

---
+++ client/src/components/TypeBar.js
@@ -0,0 +1,20 @@
+import React, {useContext} from 'react';
+import {observer} from "mobx-react-lite";
+import {Context} from "../index";
+import ListGroup from "react-bootstrap/ListGroup";
+
+const TypeBar = observer(() => {
+    const {device} = useContext(Context)
+    return (
+        <ListGroup>
+            {device.types.map(type =>
+                <ListGroup.Item style={{cursor: 'pointer'}} active={type.id === device.selectedType.id}
+                                onClick={() => device.selectedType(type)} key={type.id}>
+                    {type.name}
+                </ListGroup.Item>
+            )}
+        </ListGroup>
+    );
+});
+
+export default TypeBar;
