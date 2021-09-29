c8f40c368e7d0f4e1734c364a3d560c725a3a3d7
Mikhail Zherdev <mikhail.zherdev@storecast.de>
2021-09-29 21:00:44 +0200

    init commit. client part

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -37,7 +37,7 @@ const Auth = observer(() => {
     return (
         <Container className="d-flex justify-content-center align-items-center" style={{height: window.innerHeight - 54}}>
             <Card style={{width: 600}} className="p-5">
-                <h2 className="m-auto">{isLogin ? 'Авторизация' : "Регистрация"}</h2>
+                <h2 className="m-auto">{isLogin ? 'Login' : "Registration"}</h2>
                 <Form className="d-flex flex-column">
                     <Form.Control className="mt-3" placeholder="Enter your email" value={email} onChange={e => setEmail(e.target.value)}/>
                     <Form.Control className="mt-3" placeholder="Enter your password" value={password} onChange={e => setPassword(e.target.value)}
